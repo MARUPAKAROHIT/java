4cbc400f0b0567f412381a9f287d3daa1d515ec5
MARUPAKAROHIT <153600588+MARUPAKAROHIT@users.noreply.github.com>
2023-12-26 14:57:07 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,17 @@
+/* 
+ Program to check wheter a given number is positive or negative using ternary operator        [expression ? true option : false option]
+ Multi line comment */
+
+import java.util.Scanner;
+
+public class Positive
+{
+   public static void main(String args[])
+     {
+        System.out.print("\nEnter a number: ");
+	Scanner reader = new Scanner(System.in);
+	int num = reader.nextInt();
+	String result = num > 0 ? "Positive number" : "Negative number"; // Ternary operator
+	System.out.print("Entered number is a " + result );
+     }
+}
